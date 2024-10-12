52a78c3ceb25af1e835504460ef2c981053cff63
Stefano <30480337+snebo@users.noreply.github.com>
2024-10-12 23:11:34 +0100

    add associations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flight
+  has_many :passangers, dependent: :destroy
+
+  accepts_nested_attributes_for :passangers
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,9 @@ class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: "Airport", foreign_key: "depart_from"
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrive_at"
 
+  has_many :bookings
+  has_many :passangers, through: :bookings
+
   def same_flight
     if depart_from == arrive_at
       errors.add(:arrive_at, "Can't be the same as departing")
